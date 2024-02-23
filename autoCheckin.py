9ac5dfdbb0182326e183ef88f4814a0409bc9d29
Mateus Beppler Pereira <159061148+mateus-pereira-inteli@users.noreply.github.com>
2024-02-23 12:30:59 -0300

    correção erro click() function

--- autoCheckin.py
+++ autoCheckin.py
@@ -7,7 +7,7 @@ from win10toast import ToastNotifier
 
 def startAutoCheckin(env):
     options = Options()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
 
     driver = webdriver.Firefox(options=options)
 
@@ -27,17 +27,18 @@ def startAutoCheckin(env):
 
     time.sleep(10)
 
-    checkinButton = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/header/ul/ul/li[1]/button')
-    checkinButton.click()
+    checkinButton = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/header/ul/ul/li[1]/button').click()
     time.sleep(5)
 
     toaster = ToastNotifier()
     try:
-        comfirmCheckin = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div[6]/div/button[1]')
-        comfirmCheckin.click()
+        comfirmCheckin = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div[6]/div/button[1]').click()
         toaster.show_toast("Sucesso!", "Checkin realizado com sucesso.", duration=10)
         time.sleep(10)
     except:
-        toaster.show_toast("Erro!", "Erro ao realizar o checkin.", duration=10)
+        # Retorno erros Adalove
+        message = "Erro ao realizar o checkin."
+        toaster.show_toast("Erro!", message, duration=10)
 
     driver.quit()
+
